75e13698b3c68ee6400bef230b5632299f5a3bea
jhara islam <103064101+jharaislam19@users.noreply.github.com>
2022-09-15 13:26:42 +0600

    add blog page

--- script.js
+++ script.js
@@ -50,6 +50,7 @@ const typeController = (e) => {
     display.innerHTML += `<span class="green">${newLetter === " " ? "▪" : newLetter}</span>`;
   } else {
     display.innerHTML += `<span class="red">${newLetter === " " ? "▪" : newLetter}</span>`;
+    errorCount++;
   }
 
   // check if given question text is equal to user typed text
@@ -71,7 +72,7 @@ const gameOver = () => {
   // the current time is the finish time
   // so total time taken is current time - start time
   const finishTime = new Date().getTime();
-  const timeTaken = (finishTime - startTime) / 1000;
+  const timeTaken = Math.round((finishTime - startTime) / 1000);
 
   // show result modal
   resultModal.innerHTML = "";
@@ -142,7 +143,7 @@ displayHistory();
 // Show typing time spent
 setInterval(() => {
   const currentTime = new Date().getTime();
-  const timeSpent = parseInt((currentTime - startTime) / 1000);
+  const timeSpent = Math.round((currentTime - startTime) / 1000);
 
   
   //const showTimeParse = parseInt(showTime);
